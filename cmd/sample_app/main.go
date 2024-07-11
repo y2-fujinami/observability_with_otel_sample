5a39b1bec97e9d9656807bb46699504f0e5715c3
fnami0316 <fnami0316@gmail.com>
2024-07-11 16:53:57 +0900

    Cloud Runのデフォルトリスンポートを8080へ変更

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -8,20 +8,22 @@ import (
 	pb "modern-dev-env-app-sample/internal/sample_app/pb/api/proto"
 	"modern-dev-env-app-sample/internal/sample_app/service/sample"
 
+	"github.com/kelseyhightower/envconfig"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	listenProtocol := "tcp"
-	listenPortNum := 53000 // ポート番号
+	envVars := LoadEnvironmentVariables()
 
 	// 1. 指定したプロトコル・ポートのListenerを作成
 	// (net.Listenerが返される。Listenerとはポートに対して聞き耳を立てる人である)。
-	portListener, err := net.Listen(listenProtocol, fmt.Sprintf(":%d", listenPortNum))
+	portListener, err := net.Listen(listenProtocol, fmt.Sprintf(":%d", envVars.Port))
 	// Listenに失敗したならばプログラムを即終了
 	if err != nil {
-		log.Fatalf("プロトコル:%v, ポート:%v のListener作成に失敗しました: %v", listenProtocol, listenPortNum, err)
+		log.Fatalf("プロトコル:%v, ポート:%v のListener作成に失敗しました: %v", listenProtocol, envVars.Port, err)
 	}
 
 	// 2. gRPCサーバのインスタンスを生成
@@ -40,4 +42,19 @@ func main() {
 
 	// 5. gRPCサーバーを起動(指定したプロトコル・ポートのListenも開始)
 	grpcServer.Serve(portListener)
+}
+
+// EnvironmentVariables 環境変数
+type EnvironmentVariables struct {
+	// Port リスンポート番号
+	Port int `default:"8080"`
+}
+
+// LoadEnvironmentVariables 環境変数を読み込む
+func LoadEnvironmentVariables() EnvironmentVariables {
+	var envVars EnvironmentVariables
+	if err := envconfig.Process("", &envVars); err != nil {
+		log.Fatalf("環境変数の読み込みに失敗しました: %v", err)
+	}
+	return envVars
 }
