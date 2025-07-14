7cfd7b2343e33a022124de2590c949efac84a8ba
Yuki Fujinami <y2-fujinami@bns-g.com>
2025-07-14 14:04:02 +0900

    リソース修正

--- cmd/sample_app/main.go
+++ cmd/sample_app/main.go
@@ -11,8 +11,10 @@ import (
 	"google.golang.org/grpc/reflection"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
@@ -42,7 +44,7 @@ func main() {
 	// リソースのセットアップ
 	res, err := newResource(ctx)
 	if err != nil {
-		log.Fatal("failed to newResource(): %v", err)
+		log.Fatalf("failed to newResource(): %v", err) // TODO 終了しなくていいんだっけ？
 	}
 
 	// インフラ層のインスタンスを生成
@@ -128,3 +130,23 @@ func newResource(ctx context.Context) (*resource.Resource, error) {
 	}
 	return res, nil
 }
+
+// トレーサープロバイダのセットアップ
+func initTracerProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn) (func(context.Context) error, error) {
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	return tracerProvider.Shutdown, nil
+}
+
+// 
