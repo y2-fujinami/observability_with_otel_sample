f66861297cc8d270eafbc09f33571c0ab466a051
Yuki Fujinami <y2-fujinami@bns-g.com>
2025-07-18 16:54:24 +0900

    terraform 自体のバージョンと google provider のバージョンを更新

--- deploy/terraform/versions.tf
+++ deploy/terraform/versions.tf
@@ -1,12 +1,12 @@
 terraform {
   # terraform自体のバージョン制約
-  required_version = "1.9.1"
+  required_version = "1.12.2"
 
   # 使用するproviderとバージョン制約
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.36.0"
+      version = "6.44.0"
     }
   }
 }
